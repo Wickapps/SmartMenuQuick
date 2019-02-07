018c9c5d026aad0d7e6de186a300c4edf985de3f
Wickapps <markjw@uw.edu>
2019-02-07 17:59:29 -0600

    Initial commit

---
+++ app/src/main/java/com/wickapps/android/smartmenuquick/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wickapps.android.smartmenuquick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
