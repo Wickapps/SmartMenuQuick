785ee678a9976e79c4926a79e35641629eb7f71e
Wickapps <markjw@uw.edu>
2019-03-09 15:02:58 -0600

    Working Exit fix

--- app/src/main/java/com/wickapps/android/smartmenuquick/SplashActivity.java
+++ app/src/main/java/com/wickapps/android/smartmenuquick/SplashActivity.java
@@ -24,6 +24,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
@@ -119,9 +120,21 @@ public class SplashActivity extends Activity {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
             // Setup the ActionBar
-            getActionBar().setDisplayShowTitleEnabled(true);
-            getActionBar().setSubtitle(Global.AppName);
+            //getActionBar().setDisplayShowTitleEnabled(true);
+            //getActionBar().setSubtitle(Global.AppName);
             //getActionBar().setTitle(Global.CustomerName + " " + Global.StoreID);
+
+            // Set up with no ActionBar
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                                | View.INVISIBLE);
+            }
 
             setContentView(R.layout.splash);
 
